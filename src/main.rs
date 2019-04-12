bbb698c8060df6d77302c87c0ef7c9e8fac86794
Alex Crichton <alex@alexcrichton.com>
2019-04-12 07:18:44 -0700

    Update Cargo and move to 2018 edition

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
-extern crate cargo;
-extern crate docopt;
-extern crate env_logger;
-extern crate pathdiff;
-extern crate toml;
-#[macro_use]
-extern crate failure;
-#[macro_use]
-extern crate serde_derive;
-
 use cargo::core::{Dependency, Source};
-use cargo::core::{Workspace, SourceId, GitReference};
+use cargo::core::{GitReference, SourceId, Workspace};
 use cargo::ops;
 use cargo::util::errors::*;
 use cargo::util::important_paths::find_root_manifest_for_wd;
 use cargo::util::paths;
 use cargo::util::{Config, ToUrl};
-use toml::Value;
 use docopt::Docopt;
+use failure::{bail, format_err};
+use serde::Deserialize;
+use toml::Value;
 
 #[derive(Deserialize)]
 struct Options {
@@ -88,13 +80,15 @@ https://github.com/alexcrichton/cargo-edit-locally
 }
 
 fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
-    config.configure(options.flag_verbose,
-                     options.flag_quiet,
-                     &options.flag_color,
-                     /* frozen = */ false,
-                     /* locked = */ false,
-                     /* target_dir = */ &None,
-                     /* unstable features = */ &[])?;
+    config.configure(
+        options.flag_verbose,
+        options.flag_quiet,
+        &options.flag_color,
+        /* frozen = */ false,
+        /* locked = */ false,
+        /* target_dir = */ &None,
+        /* unstable features = */ &[],
+    )?;
 
     // Load up and resolve the crate. This'll do the whole 'Updateing registry'
     // thing in Cargo, creating a lock file if one doesn't exist or reading it
@@ -104,9 +98,7 @@ fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
         None => find_root_manifest_for_wd(config.cwd())?,
     };
     let ws = Workspace::new(&manifest, config)?;
-    let (_packages, resolve) = cargo::ops::resolve_ws(&ws).chain_err(|| {
-        "failed resolve crate"
-    })?;
+    let (_packages, resolve) = cargo::ops::resolve_ws(&ws).chain_err(|| "failed resolve crate")?;
 
     let to_replace = resolve.query(&options.arg_spec)?;
 
@@ -114,9 +106,10 @@ fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
         let path = paths::normalize_path(&config.cwd().join(p));
         SourceId::for_path(&path)?
     } else {
-        let url = options.flag_git.ok_or_else(|| {
-            format_err!("either --git or --path must be specified")
-        })?.to_url()?;
+        let url = options
+            .flag_git
+            .ok_or_else(|| format_err!("either --git or --path must be specified"))?
+            .to_url()?;
         let reference = if let Some(b) = options.flag_branch {
             GitReference::Branch(b)
         } else if let Some(t) = options.flag_tag {
@@ -129,19 +122,20 @@ fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
         SourceId::for_git(&url, reference)?
     };
 
-    let mut source = replace_with.load(config)?;
+    let mut source = replace_with.load(config, &Default::default())?;
     source.update()?;
 
     let req = format!("={}", to_replace.version().to_string());
-    let dependency = Dependency::parse_no_deprecated(&to_replace.name(),
-                                                     Some(&req),
-                                                     &replace_with)?;
+    let dependency =
+        Dependency::parse_no_deprecated(&to_replace.name(), Some(&req), replace_with)?;
     let candidates = source.query_vec(&dependency)?;
     if candidates.len() == 0 {
-        let mut msg = format!("failed to find `{} v{}` inside of `{}`\n",
-                              to_replace.name(),
-                              to_replace.version(),
-                              replace_with);
+        let mut msg = format!(
+            "failed to find `{} v{}` inside of `{}`\n",
+            to_replace.name(),
+            to_replace.version(),
+            replace_with
+        );
         if replace_with.is_git() {
             msg.push_str(&format!("perhaps a different branch/tag is needed?"));
         } else {
@@ -151,13 +145,15 @@ fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
     }
 
     let crates_io = SourceId::crates_io(config)?;
-    let to_replace_spec = if *to_replace.source_id() == crates_io {
+    let to_replace_spec = if to_replace.source_id() == crates_io {
         format!("{}:{}", to_replace.name(), to_replace.version())
     } else {
-        format!("{}#{}:{}",
-                to_replace.source_id().url(),
-                to_replace.name(),
-                to_replace.version())
+        format!(
+            "{}#{}:{}",
+            to_replace.source_id().url(),
+            to_replace.name(),
+            to_replace.version()
+        )
     };
 
     let replace_line = if replace_with.is_git() {
@@ -167,10 +163,12 @@ fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
             GitReference::Tag(ref t) => format!(", tag = \"{}\"", t),
             GitReference::Rev(ref r) => format!(", rev = \"{}\"", r),
         };
-        format!("{} = {{ git = {}{} }}\n",
-                Value::String(to_replace_spec.clone()),
-                Value::String(replace_with.url().to_string()),
-                git_extra)
+        format!(
+            "{} = {{ git = {}{} }}\n",
+            Value::String(to_replace_spec.clone()),
+            Value::String(replace_with.url().to_string()),
+            git_extra
+        )
     } else {
         let absolute_path = replace_with.url().to_file_path().unwrap();
         let relative = pathdiff::diff_paths(&absolute_path, ws.root());
@@ -180,33 +178,47 @@ fn real_main(options: Options, config: &mut Config) -> CargoResult<()> {
         } else {
             path = &absolute_path;
         }
-        format!("{} = {{ path = {} }}\n",
-                Value::String(to_replace_spec.clone()),
-                Value::String(path.display().to_string()))
+        format!(
+            "{} = {{ path = {} }}\n",
+            Value::String(to_replace_spec.clone()),
+            Value::String(path.display().to_string())
+        )
     };
     let manifest_path = ws.root().join("Cargo.toml");
     let mut manifest = paths::read(&manifest_path)?;
 
     match manifest.find("\n[replace]") {
-        Some(i) => {
-            match manifest[i + 1..].find("\n") {
-                Some(j) => manifest.insert_str(i + 2 + j, &replace_line),
-                None => {
-                    manifest.push_str("\n");
-                    manifest.push_str(&replace_line);
-                }
+        Some(i) => match manifest[i + 1..].find("\n") {
+            Some(j) => manifest.insert_str(i + 2 + j, &replace_line),
+            None => {
+                manifest.push_str("\n");
+                manifest.push_str(&replace_line);
             }
-        }
+        },
         None => {
             if manifest.contains("[replace]") {
-                bail!("don't know how to auto-modify `{}`",
-                      manifest_path.display())
+                bail!(
+                    "don't know how to auto-modify `{}`",
+                    manifest_path.display()
+                )
             }
 
-            if manifest.split('\n').rev().take_while(|s| s.trim().is_empty()).count() == 0 {
+            if manifest
+                .split('\n')
+                .rev()
+                .take_while(|s| s.trim().is_empty())
+                .count()
+                == 0
+            {
                 manifest.push_str("\n");
             }
-            if manifest.split('\n').rev().take_while(|s| s.trim().is_empty()).count() == 1 {
+            if manifest
+                .split('\n')
+                .rev()
+                .take_while(|s| s.trim().is_empty())
+                .count()
+                == 1
+            {
                 manifest.push_str("\n");
             }
             manifest.push_str("[replace]\n");
